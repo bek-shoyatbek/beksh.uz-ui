cc2aa4df78a01ce222ebeefc09481ef067dd0a45
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-18 15:01:32 +0500

    feat: Header component completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/animes">Animes</Link></li>
-        </ul>
+      <nav className={styles.nav}>
+        <NavLink to="/" className={({ isActive }) => isActive ? styles.active : ''}>Home</NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? styles.active : ''}>Projects</NavLink>
+        <NavLink to="/blog" className={({ isActive }) => isActive ? styles.active : ''}>Blog</NavLink>
+        <NavLink to="/animes" className={({ isActive }) => isActive ? styles.active : ''}>Animes</NavLink>
       </nav>
     </header>
   );
